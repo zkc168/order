19081a747c812c258a58841f1439f57f74306883
zkc168 <467805767@qq.com>
2020-01-18 09:52:57 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -32,13 +32,13 @@ rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
-# cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
-# sh $TOMCAT_APP_PATH/bin/startup.sh
+sh $TOMCAT_APP_PATH/bin/startup.sh
 
 
 
